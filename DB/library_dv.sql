3c6bde01982015de3218f18958964039a8566335
Solaris-99 <e.cifuentes996@gmail.com>
2024-07-16 16:04:05 -0300

    db creation script

---
+++ DB/library_dv.sql
@@ -0,0 +1,139 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema library_dv
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `library_dv` ;
+
+-- -----------------------------------------------------
+-- Schema library_dv
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `library_dv` DEFAULT CHARACTER SET utf8 ;
+USE `library_dv` ;
+
+-- -----------------------------------------------------
+-- Table `author`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `author` ;
+
+CREATE TABLE IF NOT EXISTS `author` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `publisher`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `publisher` ;
+
+CREATE TABLE IF NOT EXISTS `publisher` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book` ;
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `year` INT NOT NULL,
+  `id_author` INT NOT NULL,
+  `id_publisher` INT NOT NULL,
+  `total_copies` INT NOT NULL DEFAULT 0,
+  `available_copies` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_book_author`
+    FOREIGN KEY (`id_author`)
+    REFERENCES `author` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_publisher1`
+    FOREIGN KEY (`id_publisher`)
+    REFERENCES `publisher` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  `surname` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(20) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `dni` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `email_UNIQUE` ON `user` (`email` ASC) VISIBLE;
+
+
+-- -----------------------------------------------------
+-- Table `employee`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `employee` ;
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_user` INT NOT NULL,
+  `salary` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_employee_user1`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lend`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lend` ;
+
+CREATE TABLE IF NOT EXISTS `lend` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_book` INT NOT NULL,
+  `id_user` INT NOT NULL,
+  `id_employee` INT NOT NULL,
+  `time` DATETIME NOT NULL,
+  `returned` TINYINT NULL DEFAULT 0,
+  `return_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_lend_user1`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lend_employee1`
+    FOREIGN KEY (`id_employee`)
+    REFERENCES `employee` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lend_book1`
+    FOREIGN KEY (`id_book`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
